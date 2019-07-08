560c86349a22c5b4cbff1af70f7cc5366a22304d
asus <asus@DESKTOP-U322PR6>
2019-07-08 09:28:48 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package helloworld;
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.println("Hello World,¸Ä¸öĞ¡ÃÛ");
+	System.out.println("Hello World,æ”¹ä¸ªå°èœœ");
 	System.out.println("Cry On My Shoulder");
 	}
 }
